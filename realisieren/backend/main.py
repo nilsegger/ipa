62e1752f1085b367a75683e285038868a37200c7
nils <nils.egger@avectris.ch>
2020-03-25 13:06:06 +0100

    Erstellte die login route

--- realisieren/backend/main.py
+++ realisieren/backend/main.py
@@ -1,1 +1,21 @@
-from tedious.asgi.starlette import StarletteApp
+import tedious.config
+from tedious.asgi.starlette import ResourceController, StarletteApp
+from tedious.res.auth_resource import AuthResource
+from tedious.sql.postgres import PostgreSQLDatabase
+from starlette.routing import Route
+
+tedious.config.load_config('config.ini')
+
+controller = ResourceController(
+    PostgreSQLDatabase(**tedious.config.CONFIG["DB_CREDENTIALS"]))
+
+auth = AuthResource()
+
+
+async def login(request):
+    return await controller.handle(request, auth)
+
+
+app = StarletteApp(controller, [
+    Route('/login', login, methods=["POST", "PUT", "DELETE"])
+]).app
